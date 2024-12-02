22f1cc0b08b8f566444bfea95c0de444a75b54ce
praneet <praneetkashyap001@gmail.com>
2024-12-02 15:10:00 +0530

    lec 5

---
+++ demo11/operations.js
@@ -0,0 +1,7 @@
+let name = "Praneet Kashyap"
+// let revName = ""
+// let n = name.length
+// for(let i=0 ; i<name.length; i++, n--){
+//     revName.charAt(i) = name.charAt(n)
+// }
+console.log(name.split("").reverse().join(""))
