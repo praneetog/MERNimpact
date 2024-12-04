85447175d07fe79e84b69fe12ffbb2a82880f997
praneet <praneetkashyap001@gmail.com>
2024-12-04 14:01:19 +0530

    sorting done

--- demo11/sorting.js
+++ demo11/sorting.js
@@ -11,3 +11,23 @@ for(let i = 0; i < arr.length; i++){
     }
 }
 console.log(arr)
+
+//Sorting in descending order
+for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+        if(arr[i] < arr[j]){
+            let temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+        }
+    }
+}
+console.log(arr)
+
+
+//Same can be written as
+
+arr.sort((a, b) => a - b) //Sorting in ascending order
+console.log(arr)
+arr.sort((a, b) => b - a) //Sorting in descending order
+console.log(arr)
