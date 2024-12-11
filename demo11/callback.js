9a045342824a693dc3bac478927b7eabcce3dc07
praneet <praneetkashyap001@gmail.com>
2024-12-11 11:11:05 +0530

    for each

---
+++ demo11/callback.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    console.log('1');
+    setTimeout(() => {
+        console.log('2');
+        setTimeout(() => {
+            console.log('3');
+            setTimeout(() => {
+                console.log('4');
+            }, 1000)
+        }, 2000)
+    }, 3000)
+}, 4000)
