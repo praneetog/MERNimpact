e336956a360f52556c28aaf9a57ef6df5c24c854
praneet <praneetkashyap001@gmail.com>
2024-12-30 16:19:44 +0530

    mongodb demo

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import mongoose, { mongo } from "mongoose"
 const PORT = 8888
-const mongoURL = "mongodb://localhost:27017/demo"
+const mongoURL = "mongodb://localhost:27017/demo2"
 
 mongoose.connect(mongoURL)
 .then(() => {
@@ -14,11 +14,30 @@ const app = express()
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number,
-    email: String,
+    dep: String
 })
 
-mongoose.model("user", userSchema)
+const userModel = mongoose.model("users", userSchema)
+
+app.get("/api/users", async(req, res) => {
+    const users = await userModel.find()
+    res.send(users)
+})
+
+app.get("/api/users/:id", async(req, res) => {
+    const id = Number(req.params.id)
+    const user = await userModel.findOne({id : id})
+    console.log(user)
+    if(user){
+        res.json(user)
+    } else {
+        res.json({"Status" : "User not found"})
+    }
+})
+
+app.post("/api/users/add", (req, res) => {
+
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on http://localhost:${PORT}`)
